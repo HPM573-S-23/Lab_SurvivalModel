f4856816759a876c1f5fa1912b2fa762d2c99ca8
Reza Yaesoubi <reza.yaesoubi@yale.edu>
2019-01-10 15:22:27 -0500

    P3

--- SurvivalModelClasses.py
+++ SurvivalModelClasses.py
@@ -38,3 +38,37 @@ class Patient:
             # increment time
             t += 1
 
+
+class Cohort:
+    def __init__(self, id, pop_size, mortality_prob):
+        """ create a cohort of patients
+        :param id: cohort ID
+        :param pop_size: population size of this cohort
+        :param mortality_prob: probability of death for each patient in this cohort over a time-step (must be in [0,1])
+        """
+        self.patients = []      # list of patients
+        self.survivalTimes = []  # list to store survival time of each patient
+        self.meanSurvivalTime = None  # mean survival time
+
+        # populate the cohort
+        for i in range(pop_size):
+            # create a new patient (use id * pop_size + n as patient id)
+            patient = Patient(id=id * pop_size + i, mortality_prob=mortality_prob)
+            # add the patient to the cohort
+            self.patients.append(patient)
+
+    def simulate(self, n_time_steps):
+        """ simulate the cohort of patients over the specified number of time-steps
+        :param n_time_steps: number of time steps to simulate the cohort
+        """
+        # simulate all patients
+        for patient in self.patients:
+            # simulate
+            patient.simulate(n_time_steps)
+            # record survival time
+            survival_time = patient.survivalTime
+            if not (survival_time is None):
+                self.survivalTimes.append(survival_time)
+
+        # calculate mean survival time
+        self.meanSurvivalTime = sum(self.survivalTimes)/len(self.survivalTimes)
