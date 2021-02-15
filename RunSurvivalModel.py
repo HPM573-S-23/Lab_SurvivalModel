7288125e87c849e3cffecfedab752f9673ebe862
Reza Yaesoubi <reza.yaesoubi@yale.edu>
2021-02-15 11:11:36 -0500

    p3

--- RunSurvivalModel.py
+++ RunSurvivalModel.py
@@ -7,7 +7,7 @@ TIME_STEPS = 40         # years
 myCohort = Cohort(id=1, pop_size=100, mortality_prob=MORTALITY_PROB)
 
 # simulate the cohort over the specified time steps
-myCohort.simulate(TIME_STEPS)
+myCohort.simulate(n_time_steps=TIME_STEPS)
 
 # print the patient survival time
 print(myCohort.meanSurvivalTime)
