3c28d349a0cec02d100b52dc0b55bf0a647214f0
Reza Yaesoubi <reza.yaesoubi@yale.edu>
2021-02-15 11:11:21 -0500

    p3

--- RunSurvivalModel.py
+++ RunSurvivalModel.py
@@ -1,10 +1,10 @@
-import SurvivalModelClasses as Cls
+from SurvivalModelClasses import Cohort
 
 MORTALITY_PROB = 0.1    # annual probability of death
 TIME_STEPS = 40         # years
 
 # create a cohort
-myCohort = Cls.Cohort(id=1, pop_size=100, mortality_prob=MORTALITY_PROB)
+myCohort = Cohort(id=1, pop_size=100, mortality_prob=MORTALITY_PROB)
 
 # simulate the cohort over the specified time steps
 myCohort.simulate(TIME_STEPS)
